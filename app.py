f49dcc8fb8d7e1acf3cbf9bb45c07f386bb338e4
ashita03 <ashitashetty1@gmail.com>
2024-10-14 19:27:59 -0400

    update streamlit networking page

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import streamlit as st
 main_page = st.Page("navigation/main.py", title="Home", icon="🏠", default=True,)
 resume_review_page = st.Page("navigation/resume_review.py", title="Resume review", icon="📄",)
 cover_letter_page = st.Page("navigation/cover_letter.py", title="Cover Letter", icon="✍🏻",)
+networking_page = st.Page("navigation/network.py", title="Networking", icon="👩‍💻",)
 
 pg = st.navigation({"Info":[main_page],
-                    "Features":[resume_review_page, cover_letter_page]})
+                    "Features":[resume_review_page, cover_letter_page, networking_page]})
 
 
 
